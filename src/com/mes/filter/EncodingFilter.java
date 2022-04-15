b937687ad73f82fe941401f96353db78610ac41d
bizyw2353 <bizyw2353@gmail.com>
2022-04-15 10:54:38 +0900

    EncodingFilter관련부분 수정

--- src/com/mes/filter/EncodingFilter.java
+++ src/com/mes/filter/EncodingFilter.java
@@ -6,18 +6,24 @@ import java.io.IOException;
 
 @WebFilter(filterName = "encodingFilter")
 public class EncodingFilter implements Filter {
+    private String encoding;
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        encoding = filterConfig.getInitParameter("encoding");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        servletResponse.setContentType("application/json;charset=UTF-8");
-        servletResponse.setCharacterEncoding("UTF-8");
-        servletRequest.setCharacterEncoding("UTF-8");
+        if(servletRequest.getCharacterEncoding() == null || servletResponse.getCharacterEncoding() == null){
+            servletRequest.setCharacterEncoding(encoding);
+            servletResponse.setCharacterEncoding(encoding);
+        }
         filterChain.doFilter(servletRequest,servletResponse);
+
+//        servletResponse.setContentType("application/json;charset=UTF-8");
+//        servletResponse.setCharacterEncoding("UTF-8");
+//        servletRequest.setCharacterEncoding("UTF-8");
+//        filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
